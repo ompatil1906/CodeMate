b8db905e703df10c83eb637f30ff90c23e65f4af
Om patil <patilom1906@gmail.com>
2025-03-01 22:54:40 +0530

    Update

--- src/sidebarView.ts
+++ src/sidebarView.ts
@@ -59,7 +59,7 @@ export class CodeMateSidebarProvider implements vscode.WebviewViewProvider {
     return `
       <html>
         <head>
-    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/marked/9.0.3/marked.min.js"></script>
         </head>
         <body>
             <div id="chat"></div>
